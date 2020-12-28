bf106a5a008e8aa01c71f4f4600414405991ab84
Julio <jdelacruz94@msn.com>
2020-12-28 17:13:19 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pending-client';
+  pending: string = '';
+  pendents: string[] = [];
+
+  addPending(pending: string) {
+    console.log(pending);
+    this.pendents.push(pending);
+    this.pending = '';
+
+  }
+
 }
