a35a6fa38b84ecd161efcdde5f94b4935fc99b78
jdelacruz94 <jdelacruz94@msn.com>
2021-01-01 14:52:30 +0100

    CRUD listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,51 @@ import { Pending, PendingService } from './pending.service'
 })
 export class AppComponent {
   title = 'pending-client';
-  pending: string = '';
+  pending: String = '';
+  pendingEdit: String = '';
   pendingList: any;
-  newPending: Pending = {id:"", pending:""};
-
-  constructor(private pendingApi: PendingService) {
+  indexPending: number = -1;
   
+  constructor(private pendingApi: PendingService) {
+    
   }
-
+  
   ngOnInit() {
     this.getPending();
   }
-
+  
+  updatePending(pending: Pending, pendingUpdate: String ) {
+    console.log("Update id", pending.id, pendingUpdate);
+    let updatePending: Pending = {id:"", pending:""};
+    updatePending.id = pending.id;
+    updatePending.pending = pendingUpdate;
+    this.pendingApi.addPending(updatePending).subscribe(x => {
+      this.pending = '';
+      this.getPending();
+      this.indexPending = -1;
+    });
+  }
+  
+  editPending(item: Pending, index: number) {
+    console.log("edit item", item, index);
+    this.pendingEdit = item.pending;
+    this.indexPending = index;
+  }
+  
   addPending(pending: String) {
-    console.log("pending ", pending);
-    this.newPending.pending = pending;
-    this.pendingApi.addPending(this.newPending).subscribe(x => {
+    console.log("Add pending ", pending);
+    let newPending: Pending = {id:"", pending:""};
+    newPending.pending = pending;
+    this.pendingApi.addPending(newPending).subscribe(x => {
       this.pending = '';
       this.getPending();
     });
   }
   
-  deletePending(i: number) {
-    console.log("index", i);
-    this.pendingList.splice(i,1);
+  deletePending(item: Pending) {
+    this.pendingApi.deletePending(item).subscribe(x => {
+      this.getPending();
+    });
   }
   
   getPending() {
--- src/app/pending.service.ts
+++ src/app/pending.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PendingService {
+  url = environment.url;
 
   constructor(private http: HttpClient) { }
-  
-  public getPending() {
-    return  this.http.get("http://localhost:8080/api");
-  }
-  
-  public addPending(pending: Pending) {
-    console.log("Desde service Post ", pending);
+
+  public deletePending(pending:Pending) {
+    console.log("Desde service delete ", pending);
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     }
-    return this.http.post("http://localhost:8080/api", pending, httpOptions)
+    return this.http.delete(`${this.url}/${pending.id}`, httpOptions)
+  }
+
+
+  public getPending() {
+    return  this.http.get(this.url);
+  }
+  
+  public addPending(pending: Pending) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post(this.url, pending, httpOptions)
   }
   
 }
